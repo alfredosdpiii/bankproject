bb5eb1c61217f81d6e3863df1f5da971a5ba72a9
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-02-19 09:34:09 +0800

    fix

--- src/pages/signup/signup.component.jsx
+++ src/pages/signup/signup.component.jsx
@@ -5,10 +5,10 @@ import RegisterForm from "../../components/register/registerForm.component";
 function SignUp() {
   return (
     <>
-      <div class="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
-        <div class="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden">
-          <div class="md:flex w-full">
-            <div class="hidden md:block w-1/2 bg-indigo-500 py-10 px-10">
+      <div className="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
+        <div className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden">
+          <div className="md:flex w-full">
+            <div className="hidden md:block w-1/2 bg-indigo-500 py-10 px-10">
               <SVG />
             </div>
             <RegisterForm />
